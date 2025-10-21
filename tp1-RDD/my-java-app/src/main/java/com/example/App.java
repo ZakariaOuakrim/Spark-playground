3bd3cee03ab32d092c3b1ad55806f5a990dbbd88
= <=>
2025-10-21 20:34:31 +0100

    fix pom and RDD

--- tp1-RDD/my-java-app/src/main/java/com/example/App.java
+++ tp1-RDD/my-java-app/src/main/java/com/example/App.java
@@ -1,25 +1,26 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.storage.StorageLevel;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        SparkConf conf = new SparkConf().setAppName("MyApp").setMaster("local[*]");
-        JavaSparkContext javaSparkContext = new JavaSparkContext(conf);
-
-        JavaRDD<Double> rdd1 = javaSparkContext.parallelize(Arrays.asList(11.0, 12.0, 3.0, 14.0, 15.0));
-        JavaRDD<Double> rdd2=rdd1.map(x->x+1);
-        JavaRDD<Double> rdd3=rdd2.filter(x->{
+        SparkConf conf=new SparkConf().setAppName("TP 1 RDD").setMaster("local[*]");
+        JavaSparkContext javaSparkContext=new JavaSparkContext(conf);
+        List<Double> notes= Arrays.asList(10.0,15.5,9.5,8.0,19.0);
+        JavaRDD<Double> rdd1=javaSparkContext.parallelize(notes);
+        JavaRDD<Double> rdd2=rdd1.map(note -> note+1);
+        JavaRDD<Double> rdd3=rdd2.filter(note -> {
             System.out.println("OK");
-            return x>10;
+            return note>=10;
         });
         rdd3.persist(StorageLevel.MEMORY_ONLY());
         List<Double> result=rdd3.collect();
